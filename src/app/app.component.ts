c6074fc40fef2b4b0788e6695bf69dc21da54135
Hayden Brown <haydenbrowncinci@gmail.com>
2022-03-30 16:44:44 -0400

    removed htmlt code and replaced it with my own

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'The Bootcamp';
+  maxOn: boolean = true;
+  names: string[] = ["Ann", "Rob", "Cris", "David", "Ellen"];
+  customers: any[] = [
+    { id: 1, name: "MAX", sales: 10000, state: "OH"},
+    { id: 2, name: "P&G", sales: 100000, state: "OH"},
+    { id: 3, name: "Kroger", sales: 20000, state: "OH"}
+  ];
+
+  aString1: string = "";
+  aString2: string = "";
+
+  copy(): void {
+    this.aString2 = this.aString1;
+  }
+
+  clicked(): void {
+    if(this.maxOn){
+      this.title = "The MAXimum Bootcamp!";
+      this.maxOn = !this.maxOn;
+    } else {
+      this.maxOn = !this.maxOn;
+      this.title = "The Bootcamp"
+    }
+  } 
+
 }
